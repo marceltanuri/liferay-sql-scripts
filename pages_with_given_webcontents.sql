1c2d6abadab1a4f68464eb4c0bdc99786bd895a4
Marcel Tanuri <31805745+marceltanuri@users.noreply.github.com>
2018-12-03 20:56:03 -0200

    Create pages_with_given_webcontents.sql

---
+++ pages_with_given_webcontents.sql
@@ -0,0 +1,24 @@
+select l.plid, l.friendlyurl, l.groupid from PORTLETPREFERENCES
+inner join layout l on (l.plid = PORTLETPREFERENCES.plid) where 
+portletid like '56%' and 
+preferences like '<portlet-preferences><preference><name>articleId</name><value>22608186</value></preference><preference><name>groupId</name><value>7846272</value></preference></portlet-preferences>' or
+preferences like '<portlet-preferences><preference><name>articleId</name><value>22606664</value></preference><preference><name>groupId</name><value>7846272</value></preference></portlet-preferences>' or
+preferences like '<portlet-preferences><preference><name>articleId</name><value>22640369</value></preference><preference><name>groupId</name><value>7846272</value></preference></portlet-preferences>' or
+preferences like '<portlet-preferences><preference><name>articleId</name><value>22640521</value></preference><preference><name>groupId</name><value>7846272</value></preference></portlet-preferences>' or
+preferences like '<portlet-preferences><preference><name>articleId</name><value>22640540</value></preference><preference><name>groupId</name><value>7846272</value></preference></portlet-preferences>' or
+preferences like '<portlet-preferences><preference><name>articleId</name><value>22640502</value></preference><preference><name>groupId</name><value>7846272</value></preference></portlet-preferences>' or
+preferences like '<portlet-preferences><preference><name>articleId</name><value>22640559</value></preference><preference><name>groupId</name><value>7846272</value></preference></portlet-preferences>' or
+preferences like '<portlet-preferences><preference><name>articleId</name><value>22624898</value></preference><preference><name>groupId</name><value>7846272</value></preference></portlet-preferences>' or
+preferences like '<portlet-preferences><preference><name>articleId</name><value>22640476</value></preference><preference><name>groupId</name><value>7846272</value></preference></portlet-preferences>' or
+preferences like '<portlet-preferences><preference><name>articleId</name><value>22640489</value></preference><preference><name>groupId</name><value>7846272</value></preference></portlet-preferences>' or
+preferences like '<portlet-preferences><preference><name>articleId</name><value>22640605</value></preference><preference><name>groupId</name><value>7846272</value></preference></portlet-preferences>' or
+preferences like '<portlet-preferences><preference><name>articleId</name><value>22640798</value></preference><preference><name>groupId</name><value>7846272</value></preference></portlet-preferences>' or
+preferences like '<portlet-preferences><preference><name>articleId</name><value>22640817</value></preference><preference><name>groupId</name><value>7846272</value></preference></portlet-preferences>' or
+preferences like '<portlet-preferences><preference><name>articleId</name><value>22642104</value></preference><preference><name>groupId</name><value>7846272</value></preference></portlet-preferences>' or
+preferences like '<portlet-preferences><preference><name>articleId</name><value>22642120</value></preference><preference><name>groupId</name><value>7846272</value></preference></portlet-preferences>' or
+preferences like '<portlet-preferences><preference><name>articleId</name><value>22606548</value></preference><preference><name>groupId</name><value>7846272</value></preference></portlet-preferences>' or
+preferences like '<portlet-preferences><preference><name>articleId</name><value>22658042</value></preference><preference><name>groupId</name><value>7846272</value></preference></portlet-preferences>' or
+preferences like '<portlet-preferences><preference><name>articleId</name><value>22653637</value></preference><preference><name>groupId</name><value>7846272</value></preference></portlet-preferences>' or
+
+preferences like '<portlet-preferences><preference><name>articleId</name><value>22650284</value></preference><preference><name>groupId</name><value>22426606</value></preference></portlet-preferences>' or
+preferences like '<portlet-preferences><preference><name>articleId</name><value>22658706</value></preference><preference><name>groupId</name><value>22426606</value></preference></portlet-preferences>'
